55549db28b518ff481c31afaec38b96a4eda319e
rushabha-jain <rushabha.jain@outlook.com>
2021-03-16 23:51:57 +0530

    Added bundle loader

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -14,11 +14,15 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const dispatch = useDispatch();
   const [code, setCode] = useState("");
   const [err, setErr] = useState("");
+  const [bundlingInProgress, setBundlingInProgress] = useState(false);
   useEffect(() => {
+    if (!cell.content) return;
+    setBundlingInProgress(true);
     const timeout = setTimeout(async () => {
       const output = await bundler(cell.content);
       setCode(output.code);
       setErr(output.error);
+      setBundlingInProgress(false);
     }, 1000);
     return () => {
       clearTimeout(timeout);
@@ -34,7 +38,14 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value: string) => dispatch(updateCell(cell.id, value))}
           />
         </Resizable>
-        <Preview code={code} err={err} />
+        <div className="preview-wrapper">
+          {bundlingInProgress && (
+            <div className="bundle-loader">
+              <progress max={100} className="progress is-primary"></progress>
+            </div>
+          )}
+          {!bundlingInProgress && <Preview code={code} err={err} />}
+        </div>
       </div>
     </Resizable>
   );
